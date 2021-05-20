0497146dea3458b6d603b77148c030048ca78368
GhostIntheAfro <pleszczyns@gmail.com>
2021-05-20 04:13:56 +0200

    Beta Commit-2

--- app/src/main/java/com/pawelleszczynski/frontendessentials/CalcActivity.kt
+++ app/src/main/java/com/pawelleszczynski/frontendessentials/CalcActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
 import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_calc.*
@@ -62,46 +61,15 @@ class CalcActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
-    fun px_to_em(view: View)
-    {
-        output_px_to_em.text = ( input_text_view1.text.toString().toDouble()/input_text_view2.text.toString().toDouble() ).toString()
-    }
-    fun em_to_px(view: View)
-    {
-        output_em_to_px.text = ( input_text_view3.text.toString().toDouble()/input_text_view4.text.toString().toDouble() ).toString()
-    }
-    fun px_to_rem()
-    {
+   // fun px_to_em(view: View)
+    //{
+      //  output_px_to_em.text = ( input_text_view1.text.toString().toDouble()/input_text_view2.text.toString().toDouble() ).toString()
+    //}
+    //fun em_to_px(view: View)
+    //{
+      //  output_em_to_px.text = ( input_text_view3.text.toString().toDouble()/input_text_view4.text.toString().toDouble() ).toString()
+    //}
 
-    }
-    fun rem_to_px()
-    {
-
-    }
-    fun px_to_percent()
-    {
-
-    }
-    fun percent_to_px()
-    {
-
-    }
-    fun px_to_vw()
-    {
-
-    }
-    fun vw_to_px()
-    {
-
-    }
-    fun vh_to_px()
-    {
-
-    }
-    fun px_to_vh()
-    {
-
-    }
     override fun onSaveInstanceState(@NonNull outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putString("savedEquation1", output_px_to_em.text.toString())
