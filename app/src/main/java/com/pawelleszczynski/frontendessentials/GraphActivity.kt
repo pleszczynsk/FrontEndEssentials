3b82af5dd54a114b625f13935a054bec95ddd1e9
GhostIntheAfro <pleszczyns@gmail.com>
2022-01-31 14:20:10 +0100

    Final Version -> PAM2

--- app/src/main/java/com/pawelleszczynski/frontendessentials/GraphActivity.kt
+++ app/src/main/java/com/pawelleszczynski/frontendessentials/GraphActivity.kt
@@ -1,15 +1,29 @@
 package com.pawelleszczynski.frontendessentials
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_graph.*
 
 class GraphActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_graph)
+
+        SVGtoPNG.setOnClickListener {
+            val openURL = Intent(android.content.Intent.ACTION_VIEW)
+            openURL.data = Uri.parse("https://convertio.co/pl/svg-png/")
+            startActivity(openURL)
+        }
+
+        PNGtoSVG.setOnClickListener {
+            val openURL = Intent(android.content.Intent.ACTION_VIEW)
+            openURL.data = Uri.parse("https://convertio.co/pl/png-svg/")
+            startActivity(openURL)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean
--- app/src/main/java/com/pawelleszczynski/frontendessentials/TemplateActivity.kt
+++ app/src/main/java/com/pawelleszczynski/frontendessentials/TemplateActivity.kt
@@ -1,17 +1,32 @@
 package com.pawelleszczynski.frontendessentials
 
+import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_template.*
+import kotlinx.android.synthetic.main.dialog_update.*
 
-class TemplateActivity: AppCompatActivity() {
+
+class TemplateActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_template)
-    }
 
+        btnAdd.setOnClickListener { view ->
+
+            addRecord()
+        }
+
+        setupListofDataIntoRecyclerView()
+    }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean
     {
         menuInflater.inflate(R.menu.toolbar, menu)
@@ -52,5 +67,150 @@ class TemplateActivity: AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    /**
+     * Function is used to show the list of inserted data.
+     */
+    private fun setupListofDataIntoRecyclerView() {
+
+        if (getItemsList().size > 0) {
+
+            rvItemsList.visibility = View.VISIBLE
+            tvNoRecordsAvailable.visibility = View.GONE
+
+            // Set the LayoutManager that this RecyclerView will use.
+            rvItemsList.layoutManager = LinearLayoutManager(this)
+            // Adapter class is initialized and list is passed in the param.
+            val itemAdapter = ItemAdapter(this, getItemsList())
+            // adapter instance is set to the recyclerview to inflate the items.
+            rvItemsList.adapter = itemAdapter
+        } else {
+
+            rvItemsList.visibility = View.GONE
+            tvNoRecordsAvailable.visibility = View.VISIBLE
+        }
+    }
+
+    /**
+     * Function is used to get the Items List from the database table.
+     */
+    private fun getItemsList(): ArrayList<EmpModelClass> {
+        //creating the instance of DatabaseHandler class
+        val databaseHandler: DatabaseHandler = DatabaseHandler(this)
+        //calling the viewEmployee method of DatabaseHandler class to read the records
+        val empList: ArrayList<EmpModelClass> = databaseHandler.viewEmployee()
+
+        return empList
+    }
+
+    //Method for saving the employee records in database
+    private fun addRecord() {
+        val name = etName.text.toString()
+        val email = etEmailId.text.toString()
+        val databaseHandler: DatabaseHandler = DatabaseHandler(this)
+        if (!name.isEmpty() && !email.isEmpty()) {
+            val status =
+                databaseHandler.addEmployee(EmpModelClass(0, name, email))
+            if (status > -1) {
+                Toast.makeText(applicationContext, "Record saved", Toast.LENGTH_LONG).show()
+                etName.text.clear()
+                etEmailId.text.clear()
+
+                setupListofDataIntoRecyclerView()
+            }
+        } else {
+            Toast.makeText(
+                applicationContext,
+                "Name or Email cannot be blank",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    /**
+     * Method is used to show the Custom Dialog.
+     */
+    fun updateRecordDialog(empModelClass: EmpModelClass) {
+        val updateDialog = Dialog(this, R.style.Theme_Dialog)
+        updateDialog.setCancelable(false)
+        /*Set the screen content from a layout resource.
+         The resource will be inflated, adding all top-level views to the screen.*/
+        updateDialog.setContentView(R.layout.dialog_update)
+
+        updateDialog.etUpdateName.setText(empModelClass.name)
+        updateDialog.etUpdateEmailId.setText(empModelClass.email)
+
+        updateDialog.tvUpdate.setOnClickListener(View.OnClickListener {
+
+            val name = updateDialog.etUpdateName.text.toString()
+            val email = updateDialog.etUpdateEmailId.text.toString()
+
+            val databaseHandler: DatabaseHandler = DatabaseHandler(this)
+
+            if (!name.isEmpty() && !email.isEmpty()) {
+                val status =
+                    databaseHandler.updateEmployee(EmpModelClass(empModelClass.id, name, email))
+                if (status > -1) {
+                    Toast.makeText(applicationContext, "Record Updated.", Toast.LENGTH_LONG).show()
+
+                    setupListofDataIntoRecyclerView()
+
+                    updateDialog.dismiss() // Dialog will be dismissed
+                }
+            } else {
+                Toast.makeText(
+                    applicationContext,
+                    "Name or Email cannot be blank",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        })
+        updateDialog.tvCancel.setOnClickListener(View.OnClickListener {
+            updateDialog.dismiss()
+        })
+        //Start the dialog and display it on screen.
+        updateDialog.show()
+    }
+
+    /**
+     * Method is used to show the Alert Dialog.
+     */
+    fun deleteRecordAlertDialog(empModelClass: EmpModelClass) {
+        val builder = AlertDialog.Builder(this)
+        //set title for alert dialog
+        builder.setTitle("Delete Template")
+        //set message for alert dialog
+        builder.setMessage("Are you sure you wants to delete ${empModelClass.name}?")
+        builder.setIcon(android.R.drawable.ic_dialog_alert)
+
+        //performing positive action
+        builder.setPositiveButton("Yes") { dialogInterface, which ->
+
+            //creating the instance of DatabaseHandler class
+            val databaseHandler: DatabaseHandler = DatabaseHandler(this)
+            //calling the deleteEmployee method of DatabaseHandler class to delete record
+            val status = databaseHandler.deleteEmployee(EmpModelClass(empModelClass.id, "", ""))
+            if (status > -1) {
+                Toast.makeText(
+                    applicationContext,
+                    "Template deleted successfully.",
+                    Toast.LENGTH_LONG
+                ).show()
+
+                setupListofDataIntoRecyclerView()
+            }
+
+            dialogInterface.dismiss() // Dialog will be dismissed
+        }
+        //performing negative action
+        builder.setNegativeButton("No") { dialogInterface, which ->
+            dialogInterface.dismiss() // Dialog will be dismissed
+        }
+        // Create the AlertDialog
+        val alertDialog: AlertDialog = builder.create()
+        // Set other dialog properties
+        alertDialog.setCancelable(false) // Will not allow user to cancel after clicking on remaining screen area.
+        alertDialog.show()  // show the dialog to UI
     }
 }
